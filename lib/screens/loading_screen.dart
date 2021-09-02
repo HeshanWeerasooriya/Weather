c864b412e17bd8107d60fddb74ba29085f17c8e2
Heshan <heshan.imedha@gmail.com>
2021-09-02 08:08:29 +0530

    Print Weather Data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:weather/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -12,6 +13,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     getLocation();
+    getData();
     super.initState();
   }
 
@@ -20,6 +22,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     print(location.latitude);
     print(location.longitude);
+  }
+
+  void getData() async {
+    Response response = await get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=ca0491db415b1bcb2c7e30233445aae6'));
+    print(response.body);
   }
 
   @override
