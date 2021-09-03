8e1e81c364939543b3fa2823b857ea8f7486179a
Heshan <heshan.imedha@gmail.com>
2021-09-03 06:55:23 +0530

    Print each items of weather data 2

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather/services/location.dart';
 
+const apiKey = 'ca0491db415b1bcb2c7e30233445aae6';
+
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
 
@@ -12,31 +14,40 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  late double latitude;
+  late double longitude;
+
   @override
   void initState() {
     getLocation();
-    getData();
     super.initState();
   }
 
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    print('lat: $latitude');
+    print('lon: $longitude');
+
+    getData();
   }
 
   void getData() async {
     http.Response response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=ca0491db415b1bcb2c7e30233445aae6'));
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'));
     if (response.statusCode == 200) {
       String data = response.body;
 
-      var city = jsonDecode(data)['name'];
-      var conddition = jsonDecode(data)['weather'][0]['main'];
+      var decodeData = jsonDecode(data);
+
+      var city = decodeData['name'];
+      var condition = decodeData['weather'][0]['main'];
 
       print(city);
-      print(conddition);
+      print(condition);
     } else {
       print(response.statusCode);
     }
---
+++ lib/services/networking.dart
@@ -0,0 +1,7 @@
+class NetworkHelper {
+  NetworkHelper(this.url);
+
+  final String url;
+
+  void getData() async {}
+}
